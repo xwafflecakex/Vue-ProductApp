6d0d20de04f81843e7a0771e526d3e100742e480
xwafflecakex <nyevtukh@gmail.com>
2020-03-14 18:22:29 -0600

    upload files
    adding base plate

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+     el: '#app',
+     data: {
+          product: 'Socks',
+          describtion: 'A pair of warm, fuzzy socks',
+          image: './images/vmSocks-green-onWhite.jpg'
+     }
+});

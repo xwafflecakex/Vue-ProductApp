6ebcdfe43e8ed77b45c2a1e40f95d9a075ae36a9
xwafflecakex <nyevtukh@gmail.com>
2020-03-14 18:40:32 -0600

    v-bind

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var app = new Vue({
      data: {
           product: 'Socks',
           describtion: 'A pair of warm, fuzzy socks',
-          image: './images/vmSocks-green-onWhite.jpg'
+          image: './images/vmSocks-green-onWhite.jpg',
+          altText: 'A pair of socks',
+          linkedText: 'https://developercommunity.visualstudio.com/content/problem/83144/visual-studio-for-mac-repeatedly-crashing.html'
      }
-});
+})
